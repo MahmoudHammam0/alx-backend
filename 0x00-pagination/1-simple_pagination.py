b46bf0cdf8f5a0d04ba7effc5c73f811b1f45b1a
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-06-24 04:22:47 -0700

    1. Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -32,11 +32,13 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert type(page) is int and page > 0
-        assert type(page_size) is int and page_size > 0
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
         start_index, end_index = index_range(page, page_size)
-        data = self.dataset()
-        try:
-            return data[start_index:end_index]
-        except IndexError:
+
+        dataset = self.dataset()
+
+        if start_index >= len(dataset):
             return []
+        return dataset[start_index:end_index]
