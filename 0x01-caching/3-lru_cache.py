ebd51152a1ab6576c4ec236b06b501b5af1074e2
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-06-25 23:55:39 -0700

    3. LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -14,17 +14,17 @@ class LRUCache(BaseCaching):
         """ Add an item in the cache """
         if key is None or item is None:
             return
-        if len(self.cache_data.keys()) >= BaseCaching.MAX_ITEMS:
-            print("DISCARD: {}".format(self.record[0]))
-            self.cache_data.pop(self.record[0])
-            self.record.pop(0)
-        self.cache_data[key] = item
         if key in self.record:
             idx = self.record.index(key)
             self.record[-1], self.record[idx] =\
                 self.record[idx], self.record[-1]
         else:
             self.record.append(key)
+        self.cache_data[key] = item
+        if len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS:
+            print("DISCARD: {}".format(self.record[0]))
+            self.cache_data.pop(self.record[0])
+            self.record.pop(0)
 
     def get(self, key):
         """ Get an item by key """
