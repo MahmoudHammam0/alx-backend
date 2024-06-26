c49d3dfc808c6df34642483a17e14cc1edda7982
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-06-25 23:48:21 -0700

    3. LRU Caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,35 +1,46 @@
 #!/usr/bin/env python3
-""" LRU Caching module """
-BaseCaching = __import__('base_caching').BaseCaching
+""" LRUCache module """
+from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """ inherits from BaseCaching """
+    """ Least Recently Used caching system """
+
     def __init__(self):
-        """ Initiliaze """
+        """ initialisation """
         super().__init__()
-        self.record = []
+        # list of the keys from LRU to MRU
+        self.cache_list = []
 
     def put(self, key, item):
-        """ Add an item in the cache """
+        """ assigns to the dictionary the item value for the key key """
         if key is None or item is None:
             return
+
+        if key in self.cache_list:
+            # remove key to update its position later
+            self.cache_list.remove(key)
+
         self.cache_data[key] = item
-        if len(self.cache_data.keys()) > BaseCaching.MAX_ITEMS:
-            print("DISCARD: {}".format(self.record[0]))
-            self.cache_data.pop(self.record[0])
-            self.record.pop(0)
-        if key in self.record:
-            idx = self.record.index(key)
-            self.record[-1], self.record[idx] =\
-                self.record[idx], self.record[-1]
-        else:
-            self.record.append(key)
+
+        # put the recently visited item at the end
+        self.cache_list.append(key)
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            # get the key of the least recently used item
+            del_key = self.cache_list.pop(0)
+            # delete the item
+            self.cache_data.pop(del_key)
+            print("DISCARD:", del_key)
 
     def get(self, key):
-        """ Get an item by key """
+        """ returns the value in the dictionary that is linked to key """
         if key is None or key not in self.cache_data.keys():
             return None
-        idx = self.record.index(key)
-        self.record[-1], self.record[idx] = self.record[idx], self.record[-1]
-        return self.cache_data[key]
+
+        # update the order of the list by putting the most visited item in top
+        if key in self.cache_list:
+            self.cache_list.remove(key)
+            self.cache_list.append(key)
+
+        return self.cache_data.get(key)
