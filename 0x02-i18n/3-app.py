b756b1a86d56d6d5920876cc7a8ad7b9f03cb58a
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-02 05:14:07 -0700

    3. Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
-
 class Config:
     """ configurations class """
     LANGUAGES = ["en", "fr"]
@@ -21,7 +20,6 @@ babel = Babel(app)
 def get_locale():
     """ determine the best match with our supported languages """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
-
 
 
 @app.route('/')
